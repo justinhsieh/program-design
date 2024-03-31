e53dd37d6f43073f671083b4a810420cd7718267
Justin <124439574+justinhsieh@users.noreply.github.com>
2024-03-31 19:35:59 +0800

    Update merge sort.c

--- algorithm/merge sort.c
+++ algorithm/merge sort.c
@@ -3,32 +3,37 @@
 
 void mergesort(int *,int*,int,int);
 int main(){
-    int array[SIZE]={5,3,8,6,2,7,1,4};                                   //             53862714
-    int reg[SIZE]={0}; //register                                            //            5386 / 2714
-    mergesort(array,reg,0,SIZE-1);                                     //          53 / 86 / 27 / 14
-    for(int i=0;i<8;i++){                                                         //     5 / 3 / 8 / 6 / 2 / 7 / 1 / 4
-        printf("%d ",array[i]);                                                  //          35 / 68 / 27 / 14
-    }                                                                                      //             3568 / 1247
-    return 0;                                                                         //               12345678
+    int array[SIZE]={5,3,8,6,2,7,1,4};                              
+    int reg[SIZE]={0}; //register                                            
+    mergesort(array,reg,0,SIZE-1);                                     
+    for(int i=0;i<8;i++){                                                     
+        printf("%d ",array[i]);                                              
+    }                                                                                
+    return 0;                                                                        
 }
 void mergesort(int *array,int *reg,int front ,int end){
     int mid=(front+end)/2;
     if( front < end ){
+        //SEPERATE INTO TWO PARTS
+        
         mergesort(array,reg,front,mid);
         mergesort(array,reg,mid+1,end);
-
+        
         int leftpoint=front , rightpoint=mid+1;
         int i;
-
+        
         for(i=front;i<=end;i++){
+            //LEFT LIMIT
             if(leftpoint==mid+1){
                 reg[i]=array[rightpoint];
                 rightpoint++;
             }
+            //RIGHT LIMIT
             else if(rightpoint>end){
                 reg[i]=array[leftpoint];
                 leftpoint++;
             }
+            //COMPARE TWO PARTS
             else if(array[leftpoint] < array[rightpoint]){
                 reg[i]=array[leftpoint];
                 leftpoint++;
@@ -38,9 +43,10 @@ void mergesort(int *array,int *reg,int front ,int end){
                 rightpoint++;
             }
         }
-            for(i=front;i<=end;i++){
-                array[i]=reg[i];
-            }
+        //SAVE REG INTO ARRAY
+        for(i=front;i<=end;i++){
+            array[i]=reg[i];
+        }
     }
 }
 
